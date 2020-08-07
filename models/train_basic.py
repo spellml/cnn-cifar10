160a55d2b82dd8c028b143ef55e2a36713d27346
ResidentMario <23d9b4751ac460d7fc7000c4f5b042f8ecc65298>
2020-08-07 01:20:02 +0000

    Add initial training script.

---
+++ models/train_basic.py
@@ -0,0 +1,84 @@
+import torchvision
+from torch.utils.data import DataLoader
+import torch
+from torch import nn
+from torch import optim
+import numpy as np
+
+transform = torchvision.transforms.Compose([
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.RandomPerspective(),
+    torchvision.transforms.ToTensor()
+])
+dataset = torchvision.datasets.CIFAR10("/mnt/cifar10/", train=True, transform=transform, download=True)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+class CIFAR10Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.cnn_block_1 = nn.Sequential(*[
+            nn.Conv2d(3, 32, 3),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Dropout(0.25)
+        ])
+        self.cnn_block_2 = nn.Sequential(*[
+            nn.Conv2d(32, 32, 3),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Dropout(0.25)
+        ])
+        self.flatten = lambda inp: torch.flatten(inp, 1)
+        self.head = nn.Sequential(*[
+            nn.Linear(800, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, 10)
+        ])
+    
+    def forward(self, X):
+        X = self.cnn_block_1(X)
+        X = self.cnn_block_2(X)
+        X = self.flatten(X)
+        X = self.head(X)
+        return X
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(clf.parameters())
+
+def train():
+    NUM_EPOCHS = 10
+    for epoch in range(1, NUM_EPOCHS + 1):
+        losses = []
+
+        for i, (X_batch, y_cls) in enumerate(dataloader):
+            optimizer.zero_grad()
+
+            y = y_cls.cuda()
+            X_batch = X_batch.cuda()
+
+            y_pred = clf(X_batch)
+            loss = criterion(y_pred, y)
+            loss.backward()
+            optimizer.step()
+
+            curr_loss = loss.item()
+            if i % 200 == 0:
+                print(
+                    f'Finished epoch {epoch}/{NUM_EPOCHS}, batch {i}. Loss: {curr_loss:.3f}.'
+                )
+
+            losses.append(curr_loss)
+
+        print(
+            f'Finished epoch {epoch}. '
+            f'avg loss: {np.mean(losses)}; median loss: {np.min(losses)}'
+        )
+
+
+if name == "__main__":
+    train()
