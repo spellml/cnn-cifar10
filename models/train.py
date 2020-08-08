aa0fe4cd607d510ae688d5e2dfbac84c3c596d4a
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2020-08-08 22:16:53 +0000

    not

--- models/train.py
+++ models/train.py
@@ -56,7 +56,7 @@ transform_test = torchvision.transforms.Compose([
     torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),    
 ])
 
-download = os.path.exists("/mnt/cifar10/")
+download = not os.path.exists("/mnt/cifar10/")
 if download:
     print("CIFAR10 dataset not on disk, downloading...")
 else:
