8a20fc1d68ea6942491bc2524996affc57280beb
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2020-08-08 21:20:23 +0000

    +

--- models/train.py
+++ models/train.py
@@ -3,6 +3,8 @@ from torch.utils.data import DataLoader
 import torch
 from torch import nn
 from torch import optim
+from torch.utils.tensorboard import SummaryWriter
+
 import numpy as np
 import argparse
 from spell.metrics import send_metric
@@ -10,13 +12,14 @@ from spell.metrics import send_metric
 import os
 if not os.path.exists("/spell/checkpoints/"):
     os.mkdir("/spell/checkpoints/")
+writer = SummaryWriter('/spell/tensorboard/')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--epochs', type=int, dest='epochs', default=50)
 parser.add_argument('--batch_size', type=int, dest='batch_size', default=32)
 
 parser.add_argument('--conv1_filters', type=int, dest='conv1_filters', default=32)
-parser.add_argument('--conv2_filters', type=int, dest='conv2_filters', default=32)
+parser.add_argument('--conv2_filters', type=int, dest='conv2_filters', default=64)
 parser.add_argument('--dense_layer', type=int, dest='dense_layer', default=512)
 
 parser.add_argument('--conv1_dropout', type=float, dest='conv1_dropout', default=0.25)
@@ -25,37 +28,58 @@ parser.add_argument('--dense_dropout', type=float, dest='dense_dropout', default
 
 args = parser.parse_args()
 
+# Used for testing purposes.
+# class Args:
+#     def __init__(self):
+#         self.epochs = 50
+#         self.batch_size = 32
+#         self.conv1_filters = 32
+#         self.conv2_filters = 64
+#         self.dense_layer = 512
+#         self.conv1_dropout = 0.25
+#         self.conv2_dropout = 0.25
+#         self.dense_dropout = 0.5
+# args = Args()
 
-transform = torchvision.transforms.Compose([
+transform_train = torchvision.transforms.Compose([
     torchvision.transforms.RandomHorizontalFlip(),
-    torchvision.transforms.RandomPerspective(),
-    torchvision.transforms.ToTensor()
+    # torchvision.transforms.Lambda(lambda x: torch.tensor(np.array(x).reshape((3, 32, 32)) / 255, dtype=torch.float)),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
 ])
-dataset = torchvision.datasets.CIFAR10("/mnt/cifar10/", train=True, transform=transform, download=True)
-dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+transform_test = torchvision.transforms.Compose([
+    # torchvision.transforms.Lambda(lambda x: torch.tensor(np.array(x).reshape((3, 32, 32)) / 255, dtype=torch.float)),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),    
+])
+train_dataset = torchvision.datasets.CIFAR10("/mnt/cifar10/", train=True, transform=transform_train, download=True)
+train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False)
+test_dataset = torchvision.datasets.CIFAR10("/mnt/cifar10/", train=False, transform=transform_test, download=True)
+test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+
 
 class CIFAR10Model(nn.Module):
     def __init__(self):
         super().__init__()
         self.cnn_block_1 = nn.Sequential(*[
-            nn.Conv2d(3, args.conv1_filters, 3),
+            nn.Conv2d(3, args.conv1_filters, 3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(args.conv1_filters, args.conv1_filters, 3),
+            nn.Conv2d(args.conv1_filters, args.conv2_filters, 3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Dropout(args.conv1_dropout)
         ])
         self.cnn_block_2 = nn.Sequential(*[
-            nn.Conv2d(args.conv2_filters, args.conv2_filters, 3),
+            nn.Conv2d(args.conv2_filters, args.conv2_filters, 3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(args.conv2_filters, args.conv2_filters, 3),
+            nn.Conv2d(args.conv2_filters, args.conv2_filters, 3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Dropout(args.conv2_dropout)
         ])
         self.flatten = lambda inp: torch.flatten(inp, 1)
         self.head = nn.Sequential(*[
-            nn.Linear(800, args.dense_layer),
+            nn.Linear(args.conv2_filters * 8 * 8, args.dense_layer),
             nn.ReLU(),
             nn.Dropout(args.dense_dropout),
             nn.Linear(args.dense_layer, 10)
@@ -71,14 +95,42 @@ class CIFAR10Model(nn.Module):
 clf = CIFAR10Model()
 clf.cuda()
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(clf.parameters())
+optimizer = optim.RMSprop(clf.parameters(), lr=0.0001, weight_decay=1e-6)
+def test(epoch, num_epochs):
+    losses = []
+    n_right, n_total = 0, 0
+    clf.eval()
+    
+    for i, (X_batch, y_cls) in enumerate(test_dataloader):
+        with torch.no_grad():
+            y = y_cls.cuda()
+            X_batch = X_batch.cuda()
+
+            y_pred = clf(X_batch)
+            loss = criterion(y_pred, y)
+            losses.append(loss.item())
+            _, y_pred_cls = y_pred.max(1)
+            n_right, n_total = n_right + (y_pred_cls == y_cls.cuda()).sum().item(), n_total + len(X_batch)
+    
+    val_acc = n_right / n_total
+    val_loss = np.mean(losses)
+    send_metric("val_loss", val_loss)
+    send_metric("val_acc", val_acc)
+    writer.add_scalar("val_loss", val_loss, (len(train_dataloader) // 200 + 1) * epoch + (i // 200))
+    writer.add_scalar("val_acc", val_acc, (len(train_dataloader) // 200 + 1) * epoch + (i // 200))    
+    print(
+        f'Finished epoch {epoch}/{num_epochs} avg val loss: {val_loss:.3f}; median val loss: {np.median(losses):.3f}; '
+        f'val acc: {val_acc:.3f}.'
+    )
 
 def train():
+    clf.train()
     NUM_EPOCHS = args.epochs
+    
     for epoch in range(1, NUM_EPOCHS + 1):
         losses = []
 
-        for i, (X_batch, y_cls) in enumerate(dataloader):
+        for i, (X_batch, y_cls) in enumerate(train_dataloader):
             optimizer.zero_grad()
 
             y = y_cls.cuda()
@@ -89,19 +141,20 @@ def train():
             loss.backward()
             optimizer.step()
 
-            curr_loss = loss.item()
+            train_loss = loss.item()
             if i % 200 == 0:
                 print(
-                    f'Finished epoch {epoch}/{NUM_EPOCHS}, batch {i}. Loss: {curr_loss:.3f}.'
+                    f'Finished epoch {epoch}/{NUM_EPOCHS}, batch {i}. loss: {train_loss:.3f}.'
                 )
-                send_metric("loss", curr_loss)
+                send_metric("train_loss", train_loss)
+                writer.add_scalar("train_loss", train_loss, (len(train_dataloader) // 200 + 1) * epoch + (i // 200))
+            losses.append(train_loss)
 
-            losses.append(curr_loss)
- 
         print(
             f'Finished epoch {epoch}. '
-            f'avg loss: {np.mean(losses)}; median loss: {np.min(losses)}'
+            f'avg loss: {np.mean(losses)}; median loss: {np.median(losses)}'
         )
+        test(epoch, NUM_EPOCHS)    
         if epoch % 5 == 0:
             torch.save(clf.state_dict(), f"/spell/checkpoints/epoch_{epoch}.pth")
     torch.save(clf.state_dict(), f"/spell/checkpoints/model_final.pth")
