b9a1c2a928824dc450ecc9bb559e299d60fe5c0c
Aleksey Bilogur <aleksey@residentmar.io>
2020-10-20 19:21:44 -0400

    Finish up model server part.

--- server/serve.py
+++ server/serve.py
@@ -1,6 +1,12 @@
 import torch
 from torch import nn
 from torch import optim
+import torchvision
+
+import numpy as np
+import base64
+from PIL import Image
+import io
 
 # Inlining the model definition in the server code for simplicity. In a production setting, we
 # recommend creating a model module and importing that instead.
@@ -8,7 +14,7 @@ class CIFAR10Model(nn.Module):
     def __init__(
         self,
         conv1_filters=32, conv1_dropout=0.25,
-        conv2_filters=64, conv2_dropout=0.25,
+        conv2_filters=32, conv2_dropout=0.25,
         dense_layer=512, dense_dropout=0.5
     ):
         super().__init__()
@@ -48,8 +54,28 @@ from spell.serving import BasePredictor
 class Predictor(BasePredictor):
     def __init__(self):
         self.clf = CIFAR10Model()
-        self.clf.load_state_dict(torch.load("/model/checkpoints/epoch_20.pth"))
+        # self.clf.load_state_dict(torch.load("/model/checkpoints/epoch_20.pth"))
+        # XXX: using CPU for now because GPU doesn't work
+        self.clf.load_state_dict(torch.load("/model/checkpoints/epoch_20.pth", map_location="cpu"))
         self.clf.eval()
+        
+        self.transform_test = torchvision.transforms.Compose([
+            torchvision.transforms.Resize((32, 32)),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        ])
+        
+        self.labels = ['Airplane', 'Automobile', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
 
     def predict(self, payload):
-        return "Hello World!"
+        img = base64.b64decode(payload['image'])
+        img = Image.open(io.BytesIO(img), formats=[payload['format']])
+        img_tensor = self.transform_test(img)
+        # batch_size=1
+        img_tensor_batch = img_tensor[np.newaxis]
+        
+        scores = self.clf(img_tensor_batch)
+        class_match_idx = scores.argmax()
+        class_match = self.labels[class_match_idx]
+        
+        return {'class': class_match}
