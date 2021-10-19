32565392ffc34349f49b4886e714c1a52dd6099c
Aleksey Bilogur <aleksey@residentmar.io>
2021-10-19 18:30:51 -0400

    Fix basic training script CPU compat.

--- models/train_basic.py
+++ models/train_basic.py
@@ -7,46 +7,59 @@ import numpy as np
 from spell.metrics import send_metric
 
 import os
+
 CWD = os.environ["PWD"]
 if not os.path.exists(f"{CWD}/checkpoints/"):
     os.mkdir(f"{CWD}/checkpoints/")
 IS_GPU = torch.cuda.is_available()
 
-transform_train = torchvision.transforms.Compose([
-    torchvision.transforms.RandomHorizontalFlip(),
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-])
-train_dataset = torchvision.datasets.CIFAR10("/mnt/cifar10/", train=True, transform=transform_train, download=True)
+transform_train = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(
+            (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+        ),
+    ]
+)
+train_dataset = torchvision.datasets.CIFAR10(
+    "/mnt/cifar10/", train=True, transform=transform_train, download=True
+)
 train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=False)
 
 
 class CIFAR10Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.cnn_block_1 = nn.Sequential(*[
-            nn.Conv2d(3, 32, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, 3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Dropout(0.25)
-        ])
-        self.cnn_block_2 = nn.Sequential(*[
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Dropout(0.25)
-        ])
+        self.cnn_block_1 = nn.Sequential(
+            *[
+                nn.Conv2d(3, 32, 3, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(32, 64, 3, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2),
+                nn.Dropout(0.25),
+            ]
+        )
+        self.cnn_block_2 = nn.Sequential(
+            *[
+                nn.Conv2d(64, 64, 3, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(64, 64, 3, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2),
+                nn.Dropout(0.25),
+            ]
+        )
         self.flatten = lambda inp: torch.flatten(inp, 1)
-        self.head = nn.Sequential(*[
-            nn.Linear(64 * 8 * 8, 512),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(512, 10)
-        ])
+        self.head = nn.Sequential(
+            *[
+                nn.Linear(64 * 8 * 8, 512),
+                nn.ReLU(),
+                nn.Dropout(0.5),
+                nn.Linear(512, 10),
+            ]
+        )
 
     def forward(self, X):
         X = self.cnn_block_1(X)
@@ -68,11 +81,11 @@ def train():
     for epoch in range(1, NUM_EPOCHS + 1):
         losses = []
 
-        for i, (X_batch, y_cls) in enumerate(train_dataloader):
+        for i, (X_batch, y) in enumerate(train_dataloader):
             optimizer.zero_grad()
 
             if IS_GPU:
-                y = y_cls.cuda()
+                y = y.cuda()
                 X_batch = X_batch.cuda()
 
             y_pred = clf(X_batch)
@@ -83,15 +96,15 @@ def train():
             curr_loss = loss.item()
             if i % 200 == 0:
                 print(
-                    f'Finished epoch {epoch}/{NUM_EPOCHS}, batch {i}. Loss: {curr_loss:.3f}.'
+                    f"Finished epoch {epoch}/{NUM_EPOCHS}, batch {i}. Loss: {curr_loss:.3f}."
                 )
                 send_metric("loss", curr_loss)
 
             losses.append(curr_loss)
 
         print(
-            f'Finished epoch {epoch}. '
-            f'avg loss: {np.mean(losses)}; median loss: {np.median(losses)}'
+            f"Finished epoch {epoch}. "
+            f"avg loss: {np.mean(losses)}; median loss: {np.median(losses)}"
         )
 
         torch.save(clf.state_dict(), f"{CWD}/checkpoints/epoch_{epoch}.pth")
